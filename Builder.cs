f14c85f490434e8918f57268251586d69ba8912e
unitodev <yunitodev@gmail.com>
2024-05-27 14:56:04 +0700

    4645

--- Builder.cs
+++ Builder.cs
@@ -3,8 +3,12 @@ class Builder
 {
     static void PerformBuild ()
     {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/SampleScene.unity"};
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        // BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        // buildPlayerOptions.scenes = new[] { "Assets/SampleScene.unity"};
+        // BuildPipeline.BuildPlayer(buildPlayerOptions);
+        string[] scenes = { "Assets/SampleScene.unity" }; // Adjust with your scenes
+        string pathToDeploy = "Build/MyGame.exe"; // Adjust the path and filename
+
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 }
